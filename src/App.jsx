43b22d3e018b296b9df7d6597eed1c98115fe7e5
Niladrikrmkr <niladrikarmakar912@gmail.com>
2024-05-13 22:01:24 +0530

    changed interface

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
 
   const deleteHandler = (i) => {
     let copytask = [...mainTask];
-    copytask.splice(i,1);
+    copytask.splice(i, 1);
     setmainTask(copytask);
   };
 
@@ -27,16 +27,16 @@ const App = () => {
   if (mainTask.length > 0) {
     renderTask = mainTask.map((t, i) => {
       return (
-        <li key={i} className="flex items-center justify-between m-5">
-          <div className=" flex items-center justify-between w-2/3">
-            <h5 className="text-3xl font-semibold">{t.title}</h5>
-            <h6 className="text-xl font-semibold">{t.desc}</h6>
+        <li key={i} className=" flex items-center justify-between mb-1">
+          <div className=" flex justify-between w-2/5 ">
+            <h5 className="text-xl font-bold">{t.title}</h5>
+            <h6 className="text-lg font-semibold">{t.desc}</h6>
           </div>
           <button
             onClick={() => {
               deleteHandler(i);
             }}
-            className="bg-red-600 text-white px-4 py-2 rounded-full font-bold"
+            className="bg-red-700 text-white px-2 py-1 rounded-full font-semibold"
           >
             Delete
           </button>
@@ -46,18 +46,18 @@ const App = () => {
   }
 
   return (
-    <>
-      <h1 className="bg-zinc-800 text-white p-5 text-5xl font-semibold text-center">
+    <div className="bg-zinc-700 w-3/4 ml-40 mt-40 rounded-lg">
+      <h1 className=" text-white p-3 text-3xl font-bold text-center">
         My Todo List
       </h1>
       <form
         onSubmit={submitHandler}
-        className=" flex items-center justify-center"
+        className="p-2 flex flex-col justify-center items-center"
       >
         <input
           type="text"
-          className="border-zinc-900 border-4 m-8 px-4 py-2 text-2xl font-medium rounded-lg "
-          placeholder="Enter title here"
+          className="border-none text-center p-1 text-lg rounded-xl"
+          placeholder="Enter title"
           value={title}
           onChange={(e) => {
             settitle(e.target.value);
@@ -66,22 +66,22 @@ const App = () => {
 
         <input
           type="text"
-          className="border-zinc-900 border-4 m-8 px-4 py-2 text-2xl font-medium rounded-lg"
-          placeholder="Enter description here"
+          className="border-none text-center p-1 mt-2 mb-2 text-lg rounded-xl"
+          placeholder="Enter description"
           value={desc}
           onChange={(e) => {
             setdesc(e.target.value);
           }}
         />
-        <button className="bg-zinc-900 text-white px-4 py-3 m-5 text-2xl font-semibold rounded-full">
-          Add task
+        <button className="bg-green-700 text-white px-2 py-1 mb-1 text-lg font-semibold rounded-full">
+          Add Task
         </button>
       </form>
       <hr />
-      <div className="p-8 bg-slate-200">
+      <div className="p-8 bg-zinc-600 rounded-lg text-slate-400">
         <ul>{renderTask}</ul>
       </div>
-    </>
+    </div>
   );
 };
 
